3dce495d2663bf1e868180a0fee04ec93c2db488
Javier González <javigon258@gmail.com>
2025-04-10 17:40:03 +0000

    Subida Proyecto

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,11 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+
+
+datosNY = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+#datosNY.head()
+
+#datosNY.shape
+datosNY.describe
